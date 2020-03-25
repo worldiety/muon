3467be052306b0542350ed654f57d89ebd71a030
Torben Schinke <torben.schinke@worldiety.de>
2020-03-25 15:24:51 +0100

    removed muon-doc, which is replaced by wdydoc

--- doc/package.go
+++
@@ -1,2 +0,0 @@
-// doc contains the documentation language and transformations to various output formats.
-package doc
--- json/package.go
+++
@@ -1,4 +0,0 @@
-// json contains a limited marshaller and unmarshaller which supports polymorphic serialization based
-// on a jsonDiscriminator field. It is very limited in function and only supports a very small subset
-// of field types and combinations.
-package json
